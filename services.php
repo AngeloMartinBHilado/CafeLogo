e4d1470403d247032322c7c45d43db2407478d97
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-09-04 17:35:11 +0800

    Create services.php

---
+++ services.php
@@ -0,0 +1,28 @@
+<html>
+
+	<head>
+	
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+	
+	</head>
+	<body>
+		<div id="bound2">
+			<div id="banner">
+				<div id="logoB">
+						<div id="HandleA"></div>
+						<div id="HandleB"></div>
+						<div id="logoC"></div>
+						<div id="Coffe"></div>
+					
+				</div>
+			
+				</div>
+				<div class="nav">
+				<a href="index.php">Home</a> 
+				<a  href="about_us.php">About Us</a> 
+				<a class="active" href="services.php">Services</a> 
+				<a href="contact_us.php">Contact Us</a>
+			</div>	
+		</div>	
+	</body>
+</html>
